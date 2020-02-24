145401ebb75aabb34db1c1aa38d7b57fd8bc8c37
José Carlos Siqueira <jose.a.siqueira@capgemini.com>
2020-02-24 14:54:32 -0300

    Project created - Scaffold and AppBar

---
+++ android/app/src/main/kotlin/com/siqueira76/imc_calc_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.siqueira76.imc_calc_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
