1186da889700641a0cce98b2fb4d12f39e6c3ce0
José Carlos Siqueira <jose.a.siqueira@capgemini.com>
2020-02-24 18:17:43 -0300

    Fix Form Reset

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class _HomeState extends State<Home> {
     heightController.text = "";
     setState(() {
       _infoText = "Informe seus Dados!";
+       _formKey = GlobalKey<FormState>();
     });
   }
 
